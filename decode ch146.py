ae7c9f5e50bf7309344a6f43f5cea173140cc3c5
ishamehtaaa <mehta.isha@outlook.com>
2023-04-08 05:24:48 -0700

    Project #1 (challenge 146)

---
+++ decode ch146.py
@@ -0,0 +1,54 @@
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+#encode:
+def encode(enter, num):
+    encoded = ""
+    for i in enter:
+        for k in abc:
+            if i == k:
+                index = abc.index(k)
+                newnum = index + num
+                n = abc[newnum]
+            elif i == " ":
+                n = " "
+        encoded +=n
+    return encoded
+
+
+def decode(enter, num):
+    decoded = ""
+    for i in enter:
+        for k in abc:
+            if i == k:
+                index = abc.index(k)
+                newnum = index - num
+                n = abc[newnum]
+            elif i == " ":
+                n  = " "
+        decoded +=n
+    return decoded
+
+
+
+
+
+menu = print(" 1. Encode a string. \n 2. Decode a string. \n 3. Quit.")
+
+
+cont = True
+while cont:
+    choice = int(input("Please choose a number. "))
+    if choice == 1:
+        zonk = input("Please enter your string. ")
+        num = int(input("Choose how many you want to set it off by. "))
+        print(encode(zonk, num))
+    elif choice == 2:
+        bonk = input("Please enter your code. ")
+        num = int(input("Choose encryption number. "))
+        print(decode(bonk, num))
+    elif choice == 3:
+        cont = False
+        break
+
